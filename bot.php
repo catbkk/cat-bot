7695c36614a0568a60511d248bfe8d77a980821b
catbkk <psurasak.man@gmail.com>
2016-10-13 14:33:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
-
+$msg = new SAMMessage('This is a simple text message');
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
