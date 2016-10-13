77b98bcdcde84611cfce06be45aa8a9aca16739e
catbkk <psurasak.man@gmail.com>
2016-10-13 16:43:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,8 +27,8 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
-				'messages' => [$str1]
+				'messages' => [$str1],
+				
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
